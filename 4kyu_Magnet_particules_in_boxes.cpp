dbf230317f0984335cbfee719774570725d20579
YongheeLee <38936169+YongheeLee@users.noreply.github.com>
2021-09-30 09:56:10 +0900

    Create 4kyu_Magnet_particules_in_boxes.cpp

---
+++ 4kyu_Magnet_particules_in_boxes.cpp
@@ -0,0 +1,28 @@
+#include <math.h>
+
+class Magnets
+{
+public:
+  static double unit(int k, int n)
+  {
+    double t = log(double(k)) + 2.0 * double(k) * log(double(n) + 1.0);
+    t = exp(t);
+
+    return 1.0 / t;
+  }
+
+  static double doubles(int maxk, int maxn)
+  {
+    std::cout<< maxk << " " << maxn << " "<<"\n";
+    double sum = 0.0;
+    for (int k = 1; k <= maxk; ++k)
+    {
+      for (int n = 1; n <= maxn; ++n)
+      {
+        sum += unit(k, n);
+      }
+    }
+
+    return sum;
+  }
+};
