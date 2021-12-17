6746b644f0aebbd1c85e6d76951fe6b95f31979a
Yonghee(Mark) Lee <38936169+YongheeLee@users.noreply.github.com>
2021-12-17 10:29:24 +0900

    Add files via upload

---
+++ 5Kyu_Square_Matrix_Multiplication.cpp
@@ -0,0 +1,7 @@
+#include <vector>
+
+std::vector<std::vector<int>> matrix_multiplication(std::vector<std::vector<int>> &a, std::vector<std::vector<int>> &b, size_t n){
+  // TODO: Return the result of A × B in the form of an n × n matrix
+  // NOTE: Please allocate and return matrix C
+  
+}
