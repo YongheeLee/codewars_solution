88fc678fe17de205406fabeaff36116c43fda09e
Yonghee(Mark) Lee <38936169+YongheeLee@users.noreply.github.com>
2022-01-03 10:31:35 +0900

    Create 4kyu_Matrix_Determinant.cpp

---
+++ 4kyu_Matrix_Determinant.cpp
@@ -0,0 +1,43 @@
+#include <vector>
+
+long long determinant(std::vector<std::vector<long long>> m) 
+{
+    size_t sz = m.size();
+
+    if (sz == 1)
+        return m[0][0];
+
+    long long sum = 0;
+    long long s = 1;
+    for (int i = 0; i < sz; ++i)
+    {
+        std::vector<std::vector<long long>> sub;
+
+        for (int r = 1; r < sz; ++r)
+        {
+            sub.push_back(std::vector<long long>());
+            for (int c = 0; c < sz; ++c)
+            {
+                if(c == i)
+                    continue;
+
+                sub.back().push_back(m[r][c]);
+            }
+        }
+
+        long long e = m[0][i];
+        long long subD = determinant(sub);
+        long long val = s * e * subD;
+        sum += val;
+        s *= -1;
+    }
+
+    return sum;
+}
+
+int main()
+{
+    std::vector<std::vector<long long>> data = {{1, 2},{3, 4}};
+    double vas = determinant(data);
+    return 0;
+}
